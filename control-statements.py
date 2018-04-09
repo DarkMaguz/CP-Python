c5add93162f0ebe36f78db96a7ca81cb3c75b9f2
Peter Balling <peter@knowmio.com>
2018-04-09 15:52:03 +0200

    Added control statements

---
+++ control-statements.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.6
+
+# Introduction to control statements.
+# Add one segment at a time, save, them run.
+age = int(input("Please enter your age: "))
+if age <= 0:
+	print("You can't possibly be that young!")
+elif age < 6:
+	print("Shouldn't you be in kindergarten or waring a diaper or something?")
+elif age > 6 or age < 10:
+	print("Aren't you a little to young to be in this group?")
+elif age >= 18
+	print("Shouldn't you have a job as a programmer, drive a fancy car and have sweetheart by now?")
+else
+	print("Awesome age dude!")
+
